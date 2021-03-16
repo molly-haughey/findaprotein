7457584673ee1363c28940b2984f71694f3844c3
Molly Haughey <mollyhaughey@Mollys-MacBook-Pro.local>
2021-03-16 16:33:03 -0400

    fixed

--- DNA.java
+++ DNA.java
@@ -11,14 +11,11 @@ public class DNA {
     String dna = dna3;
     int start = dna.indexOf("ATG");
     int stop = dna.indexOf("TGA");
-    if ((dna.indexOf("ATG") == 0) && (dna.indexOf("TGA") == 12)) {
-    } if (start != -1 && stop != -1 && (stop-start) % 3 == 0) {
+    if (start != -1 && stop != -1 && (stop-start) % 3 == 0) {
       String protein = dna.substring(start, stop+3);
       System.out.println("Protein: " + protein);
     } else {
       System.out.println("No such protein");
-    }
-
+      }
   }
-
 }
